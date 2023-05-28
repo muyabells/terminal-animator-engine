5530f2bfbcf7b4487d5b69b3acceca52216b4134
muyabells <muyabellsanata@protonmail.com>
2023-05-28 15:16:03 +0800

    instruction generator for testing

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { AnimatedFrame, AnimatedWidget } from "./widgets/animated.js";
 import { generateStripes } from "./generators/stripe.js";
-import { composeWidgets } from "./widgets/composer.js";
+import { Compose, composeWidgets } from "./widgets/composer.js";
 import { Player } from "./helpers/player.js";
+import { Canvas } from "terminal-canvas";
 
-const total_frames = 100;
 const FRAMES60 = 16;
 const FRAMES30 = 33;
 const FRAMES25 = 40;
@@ -12,25 +12,36 @@ const FRAMES3 = 333;
 
 // FRAME GENERATOR
 function stripeGenerator(coords: { x: number, y: number }, total_frames: number) {
-    let main_frames: AnimatedFrame[] = [];
+    const main_frames: AnimatedFrame[] = [];
     for (let i = 0; i < total_frames; i++) {
         main_frames.push({
             frame: [{
                 message: generateStripes(2, 11, 5, Math.random() * 1000), 
-                coords,
+                coords: {
+                    x: i,
+                    y: i
+                },
             }],
         });
     }
     return main_frames;
 }
 
-const composed = composeWidgets({
-    main: new AnimatedWidget(stripeGenerator({ x: 2, y: 2 }, total_frames)),
-    overlays: [
-        { start_frame: 25, animation: new AnimatedWidget(stripeGenerator({ x: 16, y: 2 }, total_frames)) },
-    ]
-});
+const total_frames = 100;
+function instructionGenerator(overlays: number) {
+    const instructions: Compose = {
+        overlays: [],
+        length: 10
+    }
+    for (let i = 0; i < overlays; i++) {
+        instructions.overlays.push(
+            { start_frame: i, animation: new AnimatedWidget(stripeGenerator({ x: 3, y: 2 }, total_frames)) }
+        )
+    }
+        
+    return instructions;
+}
 
-
-const player = new Player(20, 20);
-player.play(composed, FRAMES15);
+const composed = composeWidgets(instructionGenerator(10));
+const player = new Player(400, 200);
+// player.play(composed, FRAMES60)
