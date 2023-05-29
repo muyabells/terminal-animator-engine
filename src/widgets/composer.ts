d7b8f9c1eadc3e08378fdf35402c79706676fd3b
muyabells <muyabellsanata@protonmail.com>
2023-05-29 19:10:18 +0800

    added support for ms timing

--- src/widgets/composer.ts
+++ src/widgets/composer.ts
@@ -1,20 +1,37 @@
 import { AnimatedFrame, AnimatedWidget } from "./animated.js";
 
+/**
+ * start_frame & start_ms shouldn't be specified together, it'll mess up calculations
+ */
 type Overlay = {
-    start_frame: number,
+    start_frame?: number,
+    start_ms?: number,
     animation: AnimatedWidget
 };
 export type Compose = {
     overlays: Overlay[],
-    length: number
+    length?: number,
+    ms_length?: number
 }
 
-export function composeWidgets(widget: Compose) {
+export function composeWidgets(widget: Compose, FPS: number) {
     const fr: AnimatedFrame[] = [];
+    const frame_length = widget.ms_length 
+        ? totalMSToFrameNumber(widget.ms_length, FPS) 
+        : (widget.length ?? 0);;
 
-    for (let frame = 0; frame < widget.length; frame++) {
+    for (let frame = 0; frame < frame_length; frame++) {
         const activated_widget = widget.overlays.filter((v) => {
-            return frame >= v.start_frame && (v.animation.getFrames().length - 1) > frame;
+            if (v.start_frame !== undefined) {
+                return frame >= v.start_frame 
+                    && (v.start_frame + (v.animation.getFrames().length - 1)) > frame;
+            }
+            if (v.start_ms !== undefined) {
+                const frame_from_ms = totalMSToFrameNumber(v.start_ms, FPS)
+                return frame >= frame_from_ms
+                    && (frame_from_ms + (v.animation.getFrames().length - 1)) > frame;
+            }
+            throw Error("Didn't set the start_ms and start_frame tsk tsk tsk.")
         });
         fr.push({
             frame: activated_widget.flatMap(v => {
@@ -24,4 +41,14 @@ export function composeWidgets(widget: Compose) {
         });
     }
     return fr;
+}
+
+function frameNumberToTotalMS(frame_number: number, FPS: number) {
+    return frame_number * (1 / fpsTofpms(FPS));
+}
+function totalMSToFrameNumber(ms: number, FPS: number) {
+    return Math.floor(ms * fpsTofpms(FPS));
+}
+function fpsTofpms(FPS: number) {
+    return FPS / 1000;
 }
