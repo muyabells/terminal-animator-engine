06a0cff6e31f34d5fd9619e423d7f3f6d553fd8d
muyabells <muyabellsanata@protonmail.com>
2023-06-01 18:21:25 +0800

    added parser for files and string

---
+++ src/widgets/parser.ts
@@ -0,0 +1,37 @@
+import { AnimatedFrame } from "../index.js";
+import { Cell, Frame } from "./animated.js";
+
+export function parseAniFile(
+    ani_output: string,
+): Cell[][] [] {
+    const frame_content_regex = /\d+:\s*<START>([\s\S]*?)<END>/g;
+    const frame_matches = [...ani_output.matchAll(frame_content_regex)]
+        .map(v => v[1].trim());
+
+    const ani_frames = [];
+    for (const frame of frame_matches) {
+        const cell: Cell[][] = parseStringToCells(frame);
+        ani_frames.push(cell);
+    }
+    return ani_frames;
+}
+
+export function parseStringToCells(complete_strings: string): Cell[][] {
+    const str = complete_strings
+        .split("\n")
+        // .filter(v => v.length > 0 )
+    const cell: Cell[][] = [];
+
+    for (const y of str) {
+        const al = [];
+        for (const x of y) {
+            al.push({
+                f: x,
+                // color not supported!
+            })
+        }
+        cell.push(al)
+    }
+
+    return cell;
+}
