8a48d44fc03f337632c2ef6ed9eb131e7b0375e9
muyabells <muyabellsanata@protonmail.com>
2023-06-01 18:19:54 +0800

    converted to exported members

--- src/index.ts
+++ src/index.ts
@@ -3,44 +3,28 @@ import { generateStripes } from "./generators/stripe.js";
 import { composeWidgets, totalMSToFrameNumber } from "./widgets/composer.js";
 import { Player } from "./widgets/player.js";
 import { reverse } from "./editors/reverse.js";
+import { extend } from "./editors/extend.js";
+import { parseAniFile, parseStringToCells } from "./widgets/parser.js";
 
-const FRAMES60 = 16;
-const FRAMES30 = 33;
-const FRAMES25 = 40;
-const FRAMES15 = 66;
-const FRAMES3 = 333;
+export {
+    // essentials
+    AnimatedFrame, AnimatedWidget,
 
-// FRAME GENERATOR
-function stripeGenerator(
-    coords: { x: number, y: number }, 
-    total_frames: number,
-    seed: number
-) {
-    const main_frames: AnimatedFrame[] = [];
-    for (let i = 0; i < total_frames; i++) {
-        main_frames.push({
-            frame: [{
-                message: generateStripes(i, 12, 1, seed), 
-                coords: {
-                    x: (i + i + i + i) + coords.x,
-                    y: coords.y
-                },
-            }],
-        });
-    }
-    return main_frames;
+    // generators
+    generateStripes,
+
+    // compiles widgets
+    composeWidgets, 
+    
+    // helpers
+    totalMSToFrameNumber,
+
+    // media player
+    Player,
+
+    // frame editors
+    reverse, extend,
+
+    // parsers
+    parseAniFile, parseStringToCells
 }
-
-// refractor start_ms to a helper function
-// pass the result of that helper function to the frames category
-const composed = composeWidgets({
-    overlays: [
-        {
-            start_frame: totalMSToFrameNumber(50, FRAMES15), 
-            animation: new AnimatedWidget(reverse(stripeGenerator({ x: 3, y: 2 }, 10, 10), 30))
-        },
-    ],
-    length: totalMSToFrameNumber(100, FRAMES15)
-});
-const player = new Player(400, 100);
-player.play(composed, FRAMES15)
