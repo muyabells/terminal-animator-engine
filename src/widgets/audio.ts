a8c8d2eb88123ad30ed6e85bf85a9013e350373f
muyabells <muyabellsanata@protonmail.com>
2023-06-01 18:47:28 +0800

    audio usage and implementations

--- src/widgets/audio.ts
+++ src/widgets/audio.ts
@@ -1,26 +1,28 @@
-import fs from "fs";
-import portAudio from "naudiodon";
+import { ReadStream } from "fs";
+import { AudioIO } from "naudiodon";
 
-// Create an instance of AudioIO with outOptions (defaults are as below), which will return a WritableStream
-const ao = portAudio.AudioIO({
-  outOptions: {
-    channelCount: 2,
-    sampleFormat: portAudio.SampleFormat16Bit,
-    sampleRate: 44100,
-    deviceId: -1, // Use -1 or omit the deviceId to select the default device
-    closeOnError: true, // Close the stream if an audio error is detected, if set false then just log the error
-    framesPerBuffer: 60
-  }
-});
+export function playAudio(
+	// Create a stream to pipe into the AudioOutput
+	// Note that this does not strip the WAV header so a click will be heard at the beginning
+	stream: ReadStream,
 
-// Create a stream to pipe into the AudioOutput
-// Note that this does not strip the WAV header so a click will be heard at the beginning
-const rs = 
-  fs.createReadStream('../media/credits.wav')
-    .on("data", (s) => {
-      console.log(s);
-    })
+	sampleFormat: 8 | 16 = 16,
+	sampleRate: number = 44100
+) {
+	// Create an instance of AudioIO with outOptions (defaults are as below), which will return a WritableStream
+	const ao = AudioIO({
+		outOptions: {
+		  channelCount: 2,
+		  sampleFormat: sampleFormat,
+		  sampleRate: sampleRate,
+		  deviceId: -1, // Use -1 or omit the deviceId to select the default device
+		  closeOnError: true, // Close the stream if an audio error is detected, if set false then just log the error
+		}
+	});
 
-// Start piping data and start streaming
-rs.pipe(ao);
-ao.start();
+	// Start piping data and start streaming
+	stream.pipe(ao);
+	ao.start();
+}
+
+// const rs = fs.createReadStream('../media/credits.wav');
