7d60b3f935420fc797c1dce5522386e82b9d0981
muyabells <muyabellsanata@protonmail.com>
2023-06-02 23:10:21 +0800

    unimplemented new frame editor

---
+++ src/editors/time.ts
@@ -0,0 +1,8 @@
+import { AnimatedFrame } from "../index.js";
+
+export function modifyTime(
+    frames: AnimatedFrame[],
+    speed: number // 0.5 (slowest) => 1 (neutral) => 1.5 (fastest)
+) {
+    
+}
