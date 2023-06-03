1c08c3b57979372a7762b48a7fb87d17914dde8d
muyabells <muyabellsanata@protonmail.com>
2023-06-03 23:45:53 +0800

    typewriter effect

---
+++ src/generators/typewriter.ts
@@ -0,0 +1,10 @@
+import { AnimatedFrame } from "../index.js";
+
+export function typewriter(str: string): AnimatedFrame[] {
+    const typed = [];
+    const split_str = str.split("");
+
+    
+
+    return [];
+}
