51d9e6a279bbcdde67c97d8a8c56c9d6a656af55
muyabells <muyabellsanata@protonmail.com>
2023-06-03 23:45:41 +0800

    time.ts => temporal.ts

--- src/editors/time.ts
+++
@@ -1,8 +0,0 @@
-import { AnimatedFrame } from "../index.js";
-
-export function modifyTime(
-    frames: AnimatedFrame[],
-    speed: number // 0.5 (slowest) => 1 (neutral) => 1.5 (fastest)
-) {
-    
-}
