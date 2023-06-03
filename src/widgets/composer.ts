9cf143dec11972d142c904b70e48ef5143253545
muyabells <muyabellsanata@protonmail.com>
2023-06-03 23:46:04 +0800

    removed unnecessary error

--- src/widgets/composer.ts
+++ src/widgets/composer.ts
@@ -14,11 +14,8 @@ export function composeWidgets(widget: Compose) {
 
     for (let frame = 0; frame < widget.length; frame++) {
         const activated_widget = widget.overlays.filter((v) => {
-            if (v.start_frame !== undefined) {
-                return frame >= v.start_frame 
-                    && (v.start_frame + (v.animation.getFrames().length)) > frame;
-            }
-            throw Error("Didn't set the start_ms and start_frame tsk tsk tsk.")
+            return frame >= v.start_frame 
+                && (v.start_frame + (v.animation.getFrames().length)) > frame;
         });
         fr.push({
             frame: activated_widget.flatMap(v => {
