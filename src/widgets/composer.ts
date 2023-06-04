3791934749810e2f194c9bd88b70b41fafb7a989
muyabells <muyabellsanata@protonmail.com>
2023-06-04 19:45:13 +0800

    comment on overlays

--- src/widgets/composer.ts
+++ src/widgets/composer.ts
@@ -1,19 +1,23 @@
 import { AnimatedFrame, AnimatedWidget } from "./animated.js";
 
-type Overlay = {
+type Layer = {
     start_frame: number,
     animation: AnimatedWidget
 };
 export type Compose = {
-    overlays: Overlay[],
+    layers: Layer[],
     length: number,
 }
-
+/**
+ * The lower the overlays are, the higher "layer" they will appear
+ * @param widget AnimatedWidgets
+ * @returns AnimatedFrame
+ */
 export function composeWidgets(widget: Compose) {
     const fr: AnimatedFrame[] = [];
 
     for (let frame = 0; frame < widget.length; frame++) {
-        const activated_widget = widget.overlays.filter((v) => {
+        const activated_widget = widget.layers.filter((v) => {
             return frame >= v.start_frame 
                 && (v.start_frame + (v.animation.getFrames().length)) > frame;
         });
