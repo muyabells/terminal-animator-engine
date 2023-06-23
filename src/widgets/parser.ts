690da770becccbe592cf0776e2b98029d6edafae
muyabells <muyabellsanata@protonmail.com>
2023-06-24 01:22:51 +0800

    feat: html parser

--- src/widgets/parser.ts
+++ src/widgets/parser.ts
@@ -1,13 +1,11 @@
 import { AnimatedFrame } from "../index.js";
 import { Cell, Frame } from "./animated.js";
 
-type Color = { 
-    r: number, g: number, b: number, 
-    index: { 
-        start: { line: number, character: number },
-        end:   { line: number, character: number },
-    },
-};
+type ColorParameters = {
+    r: number,
+    g: number,
+    b: number,
+}
 
 export function parseAniFile(
     ani_output: string,
@@ -41,7 +39,6 @@ export function parseStringsToFrames(
 
 export function parseStringToCells(
     complete_strings: string,
-    ...colors: Color[]
 ): Cell[][] {
     const cell: Cell[][] = [];
 
@@ -55,10 +52,9 @@ export function parseStringToCells(
 
         for (let character_index = 0; character_index < line.length; character_index++) {
             const character = line[character_index];
-
             cell_innard.push({
                 f: character.trim(),
-                color: parseColorRGB(line_index, character_index, colors)
+                color: undefined
             });
         }
         cell.push(cell_innard);
@@ -67,32 +63,86 @@ export function parseStringToCells(
     return cell;
 }
 
-function parseColorRGB(
-    line_index: number, character_index: number,
-    colors: Color[],
-) {
-    const color = colors?.find(v => {
-        return ((v.index.start.line >= line_index) !== (v.index.start.character >= character_index))
-            && ((v.index.end.line < line_index) === (v.index.end.character < character_index));
-    });
+export function badHTMLParser(stri: string) {
+    const str_split = stri
+        .split("\n")
+        .map(v => v.replace("\r", "")); // removes duplication issue :)
 
-    return color
-        ? { r: color.r, g: color.g, b: color.b }
-        : undefined;
+    const state = {
+        applied_effects: new Map<string, string>(),
+        effect: "",
+        parameters: "",
+        // locks for adding effects into applied_effects
+        effect_add_lock: false,
+        // locks for adding parameters
+        parameter_add_lock: false,
+        // lock for end
+        is_ending: false,
+        // lock for blocking it from being added into the parsed thing
+        is_included: false,
+    }
+
+    for (let line_index = 0; line_index < str_split.length; line_index++) {
+        const line = str_split[line_index];
+        for (let character_index = 0; character_index < line.length; character_index++) {
+            const character = line[character_index];
+            characterParser(character, state);
+
+            if (state.is_included && character !== ">")
+                console.log(`${[...state.applied_effects.entries()]} => ${character}`);
+        }
+    }
 }
 
-console.log(parseStringToCells(
-    `dsa <color 1, 2>dsfds</color> <color 1, 2, 3>dsfdsdsa</color>\nmelt fndskjfn`, 
-        { r: 10, g: 10, b: 10,
-            index: {
-                start: { line: 0, character: 2 },
-                end:   { line: 0, character: 10 },
+badHTMLParser("who are you, who am <color(paraamm)> I <p(wtf)>II</p>??? </color>  aa")
+
+function characterParser(
+    character: string,
+    state: {
+        applied_effects: Map<string, string>,
+        effect: string,
+        parameters: string,
+        effect_add_lock: boolean,
+        parameter_add_lock: boolean,
+        is_ending: boolean,
+        is_included: boolean,
+    }
+) {
+    switch (character) {
+        case "/":
+            state.is_ending = true;
+            break;
+        case "<":
+            state.effect_add_lock = true;
+            state.is_ending = false;
+            state.effect = "";
+            state.is_included = false;
+            break;
+        case ">":
+            if (!state.is_ending) {
+                state.applied_effects.set(state.effect, state.parameters);
+            } else {
+                state.applied_effects.delete(state.effect);
             }
-        },
-        { r: 10, g: 10, b: 10,
-            index: {
-                start: { line: 1, character: 2 },
-                end:   { line: 1, character: 6 },
+            state.is_included = true;
+            break;
+        case "(":
+            state.effect_add_lock = false;
+            state.parameter_add_lock = true;
+            break;
+        case ")":
+            state.parameter_add_lock = false;
+            break;
+        default:
+            if (state.effect_add_lock) {
+                state.effect += character;
+                break;
             }
-        },
-));
+            if (state.parameter_add_lock) {
+                state.parameters += character;
+                break;
+            }
+            state.is_included = true;
+            break;
+    }
+}
